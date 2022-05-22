f8b0edbdf6527f72229f3cff2360952c4af39d0e
wongstephen <wongstephenky@gmail.com>
2022-05-21 23:44:35 -0700

    project 8 filter

--- 8_filter-project-main/js/app.js
+++ 8_filter-project-main/js/app.js
@@ -3,10 +3,41 @@ const btn = document.querySelectorAll('.btn')
 const storeItem = document.querySelectorAll('.store-item')
 // data filter: all, cake, cupcake, sweet, doughnuts
 
-storeItem.forEach (e => {
-    if (e.classList.contains(!'doughnut')) {
-        e.style.display = 'none'
-    }
+btn.forEach(btnClick=>{
+    btnClick.addEventListener('click', function (){
+        const filter = btnClick.dataset.filter
+        if (filter === 'all') {
+            storeItem.forEach(e=>{
+                e.style.display = 'block';
+            })
+        } else {
+            storeItem.forEach(e=>{
+                if (e.dataset.item === filter) {
+                    console.log(e.dataset.item)
+                    e.style.display = 'block'
+                } 
+                else {
+                    e.style.display = 'none'
+                }
+            })
+        }
+
+    })
 })
 
-console.log('test1')
+
+//searchbox
+
+const searchItem = document.querySelector('#search-item')
+const searchIcon = document.querySelector('#search-icon')
+searchIcon.addEventListener('click', function (){
+    const searchValue = searchItem.value.toLowerCase().trim()
+    storeItem.forEach(e => {
+        if (e.textContent.includes(searchValue)) {
+            e.style.display = 'block'
+        } else {
+            e.style.display = 'none'
+        }
+    })
+})
+
